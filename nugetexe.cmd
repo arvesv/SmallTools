969a72aab5b06328901dbe631ee808297b27768b
ArveSv <657470+arvesv@users.noreply.github.com>
2017-12-11 12:17:45 +0100

    Better fnutt management

--- nugetexe.cmd
+++ nugetexe.cmd
@@ -4,13 +4,13 @@ REM A replacement for nuget.exe to avoid checkin in binaries under source contro
 
 SETLOCAL
 
-SET nugetfolder="%~dp0.nuget"
+SET nugetfolder=%~dp0.nuget
 SET nugetpath="%nugetfolder%\nuget.exe"
 
 if not exist "%nugetfolder%" mkdir "%nugetfolder%"
 
 if not exist "%nugetpath%" (
-	echo Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile "%nugetpath%" | powershell -command -
+	echo Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile %nugetpath% | powershell -command -
 )
 
 set TMP=.
